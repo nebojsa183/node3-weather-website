5f3572c071b6bc10bf4d728180a0d602e58c9ed0
unknown <nebojsanikolic183@gmail.com>
2021-07-13 17:20:00 +0200

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,16 +8,14 @@ console.log(messageOne);
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const location = search.value;
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageTwo.textContent = data.error;
-        } else {
-          console.log(data.location);
-          messageOne.textContent = data.forcast;
-        }
-      });
-    }
-  );
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageTwo.textContent = data.error;
+      } else {
+        console.log(data.location);
+        messageOne.textContent = data.forcast;
+      }
+    });
+  });
 });
